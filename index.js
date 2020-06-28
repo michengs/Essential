1dc5732ba728b09e1a218fc8661d2e9fdcb3cc30
mc <46639858+michengs@users.noreply.github.com>
2020-06-28 19:27:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ module.exports = function Essentials(mod) {
 
 			
 			enabled = false
-			mod.command.message(niceName + '<font color="#56B4E9">你用完了</font>' + missingns + missingbp + missingcb + '<font color="#56B4E9">. 请填充库存，30s后继续尝试启动必需品模组 .</font>' )
+			mod.command.message(niceName + '<font color="#56B4E9">你用完了</font>' + missingns + missingbp + missingcb + '<font color="#56B4E9">. 请填充库存，30s后将再次尝试启动必需品模组 .</font>' )
 			return
 		}
 		if(!resetcount) resetcount = setTimeout(() => { counter = 0; resetcount = null }, 15000)
